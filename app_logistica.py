5ff1173409d407710c1c9cedf10c7ad9928abee8
mariasaco-sys <maria.saco@leroymerlin.es>
2026-01-15 11:46:36 +0100

    Refactor app_logistica.py with enhanced structure

--- app_logistica.py
+++ app_logistica.py
@@ -69,3 +69,378 @@ EJE NORTE (AG-53/A-52),32570,MASIDE,3,Martes
 EJE NORTE (AG-53/A-52),32418,RIBADAVIA,2,Martes
 EJE NORTE (AG-53/A-52),32419,"VIEITE, LEIRO",2,Martes
 EJE NORTE (AG-53/A-52),32420,LEBOSENDE- LEIRO,2,Martes
+EJE NORTE (AG-53/A-52),32428,LEIRO,2,Martes
+EJE NORTE (AG-53/A-52),32573,MASIDE,2,Martes
+EJE NORTE (AG-53/A-52),36512,LALIN,2,Martes
+EJE NORTE (AG-53/A-52),15172,OLEIROS,1,Martes
+EJE NORTE (AG-53/A-52),15176,OLEIROS,1,Martes
+EJE NORTE (AG-53/A-52),32139,BOBORAS,1,Martes
+EJE NORTE (AG-53/A-52),32141,CEA,1,Martes
+EJE NORTE (AG-53/A-52),32141,SAN CRISTOBAL DE CEA,1,Martes
+EJE NORTE (AG-53/A-52),32141,SAN CRISTOBO DE CEA,1,Martes
+EJE NORTE (AG-53/A-52),32141,SAN CRISTOVO DE CEA,1,Martes
+EJE NORTE (AG-53/A-52),32410,MELON,1,Martes
+EJE NORTE (AG-53/A-52),32411,MELON,1,Martes
+EJE NORTE (AG-53/A-52),32415,RIBADAVIA,1,Martes
+EJE NORTE (AG-53/A-52),32415,VENTOSELA (RIBADAVIA ),1,Martes
+EJE NORTE (AG-53/A-52),32416,RIBADAVIA,1,Martes
+EJE NORTE (AG-53/A-52),32416,"SAN CRISTOBAL, RIBADAVIA",1,Martes
+EJE NORTE (AG-53/A-52),32418,FRANCELOS - RIBADAVIA,1,Martes
+EJE NORTE (AG-53/A-52),32418,SAMPAIO RIBADAVIA,1,Martes
+EJE NORTE (AG-53/A-52),32418,"SANPAIO, RIBADAVIA",1,Martes
+EJE NORTE (AG-53/A-52),32420,O CARBALLIÑO,1,Martes
+EJE NORTE (AG-53/A-52),32425,LEIRO,1,Martes
+EJE NORTE (AG-53/A-52),32429,LEIRO,1,Martes
+EJE NORTE (AG-53/A-52),32500,CARBALLIÑO - ARCOS,1,Martes
+EJE NORTE (AG-53/A-52),32500,CONCELLO DE CARBALLIÑO,1,Martes
+EJE NORTE (AG-53/A-52),32500,MAGARIÑOS-O CARBALLIÑO,1,Martes
+EJE NORTE (AG-53/A-52),32510,O CARBALLIÑO,1,Martes
+EJE NORTE (AG-53/A-52),32514,"ALBARELLOS,BOBORAS",1,Martes
+EJE NORTE (AG-53/A-52),32514,BOBORAS,1,Martes
+EJE NORTE (AG-53/A-52),32514,BOBORAS VILACHA CARBALLIÑO,1,Martes
+EJE NORTE (AG-53/A-52),32515,O CARBALLIÑO,1,Martes
+EJE NORTE (AG-53/A-52),32515,PARTOVIA - O CARBALLIÑO,1,Martes
+EJE NORTE (AG-53/A-52),32520,"BARROSO, DE AVION",1,Martes
+EJE NORTE (AG-53/A-52),32534,O CARBALLIÑO,1,Martes
+EJE NORTE (AG-53/A-52),32570,O LAGO MASIDE,1,Martes
+EJE NORTE (AG-53/A-52),32573,CONCELLO DE MASIDE,1,Martes
+EJE NORTE (AG-53/A-52),32574,MASIDE,1,Martes
+EJE NORTE (AG-53/A-52),32577,"LOUREDO, MASIDE",1,Martes
+EJE NORTE (AG-53/A-52),36510,LALIN,1,Martes
+EJE NORTE (AG-53/A-52),36540,SILLEDA,1,Martes
+EJE SUR (A-52),32630,XINZO DE LIMIA,28,Miércoles
+EJE SUR (A-52),32600,VERIN,26,Miércoles
+EJE SUR (A-52),32660,ALLARIZ,25,Miércoles
+EJE SUR (A-52),32112,PADERNE DE ALLARIZ,12,Miércoles
+EJE SUR (A-52),32670,XUNQUEIRA DE AMBIA,8,Miércoles
+EJE SUR (A-52),32664,ALLARIZ,6,Miércoles
+EJE SUR (A-52),32667,ALLARIZ,6,Miércoles
+EJE SUR (A-52),32692,SANDIAS,6,Miércoles
+EJE SUR (A-52),32668,ALLARIZ,5,Miércoles
+EJE SUR (A-52),32870,LOBIOS,5,Miércoles
+EJE SUR (A-52),32610,RIOS,4,Miércoles
+EJE SUR (A-52),32892,LOBIOS,4,Miércoles
+EJE SUR (A-52),32637,XINZO DE LIMIA,3,Miércoles
+EJE SUR (A-52),32665,ALLARIZ,3,Miércoles
+EJE SUR (A-52),32678,XUNQUEIRA DE AMBIA,3,Miércoles
+EJE SUR (A-52),32689,CUALEDRO,3,Miércoles
+EJE SUR (A-52),32695,TRASMIRAS,3,Miércoles
+EJE SUR (A-52),27423,LOBIOS,2,Miércoles
+EJE SUR (A-52),32627,TINTORES VERIN,2,Miércoles
+EJE SUR (A-52),32632,XINZO DE LIMIA,2,Miércoles
+EJE SUR (A-52),32636,XINZO DE LIMIA MORGADE,2,Miércoles
+EJE SUR (A-52),32650,XINZO VILAR DE SANTOS,2,Miércoles
+EJE SUR (A-52),32678,CONCELLO DE XUNQUEIRA DE AMBIA,2,Miércoles
+EJE SUR (A-52),32680,CUALEDRO,2,Miércoles
+EJE SUR (A-52),32688,CUALEDRO,2,Miércoles
+EJE SUR (A-52),32895,LOBIOS,2,Miércoles
+EJE SUR (A-52),32111,PADERNE ALLARIZ,1,Miércoles
+EJE SUR (A-52),32111,PADERNE DE ALLARIZ,1,Miércoles
+EJE SUR (A-52),32111,SANCRISTVO PADERNE DE ALLARIZ,1,Miércoles
+EJE SUR (A-52),32112,PDERNE DE ALLARIZ,1,Miércoles
+EJE SUR (A-52),32525,ALLARIZ DOADE,1,Miércoles
+EJE SUR (A-52),32610,COVELAS RIOS,1,Miércoles
+EJE SUR (A-52),32611,RIOS,1,Miércoles
+EJE SUR (A-52),32611,VERIN,1,Miércoles
+EJE SUR (A-52),32614,VERIN,1,Miércoles
+EJE SUR (A-52),32618,MEDEIROS MONTERREI,1,Miércoles
+EJE SUR (A-52),32618,MONTERREI,1,Miércoles
+EJE SUR (A-52),32618,VERIN,1,Miércoles
+EJE SUR (A-52),32618,VILLAZA DE MONTERREI,1,Miércoles
+EJE SUR (A-52),32619,CONCELLO DE MONTERREI,1,Miércoles
+EJE SUR (A-52),32630,XINZO,1,Miércoles
+EJE SUR (A-52),32631,XINZO DE LIMIA,1,Miércoles
+EJE SUR (A-52),32635,XINZO DE LIMIA,1,Miércoles
+EJE SUR (A-52),32636,XINZO DE LIMIA,1,Miércoles
+EJE SUR (A-52),32646,"XINZO , BALTAR",1,Miércoles
+EJE SUR (A-52),32655,XINZO,1,Miércoles
+EJE SUR (A-52),32664,"PENAMA, ALLARIZ",1,Miércoles
+EJE SUR (A-52),32667,REQUEIXO DE VALVERDE / ALLARIZ,1,Miércoles
+EJE SUR (A-52),32670,XINZO DE LIMIA,1,Miércoles
+EJE SUR (A-52),32679,XUNQUEIRA DE AMBIA,1,Miércoles
+EJE SUR (A-52),32680,BALDRIZ - CONCELLO DE CUALEDRO .,1,Miércoles
+EJE SUR (A-52),32688,PEDROSA CUALEDRO,1,Miércoles
+EJE SUR (A-52),32689,CARZOA CUALEDRO,1,Miércoles
+EJE SUR (A-52),32696,"LOGOSELO, SARREAUS ,XINZO DE LIMIA",1,Miércoles
+EJE SUR (A-52),32696,TRASMIRAS,1,Miércoles
+EJE SUR (A-52),32730,XUNQUEIRA DE AMBIA,1,Miércoles
+EJE SUR (A-52),32750,XUNQUEIRA DE AMBIA,1,Miércoles
+EJE SUR (A-52),32840,LOBIOS,1,Miércoles
+EJE SUR (A-52),32869,LOBIOS,1,Miércoles
+EJE SUR (A-52),32890,LOBIOS,1,Miércoles
+EJE SUR (A-52),32892,XENDIVE (LOBIOS),1,Miércoles
+ZONA METRO,32005,OURENSE,162,Lun y Vie
+ZONA METRO,32001,OURENSE,135,Lun y Vie
+ZONA METRO,32002,OURENSE,105,Lun y Vie
+ZONA METRO,32004,OURENSE,89,Lun y Vie
+ZONA METRO,32003,OURENSE,44,Lun y Vie
+ZONA METRO,32890,BARBADAS,15,Lun y Vie
+ZONA METRO,32911,SAN CIBRAO DAS VIÑAS,13,Lun y Vie
+ZONA METRO,32710,PEREIRO DE AGUIAR,12,Lun y Vie
+ZONA METRO,32170,AMOEIRO,10,Lun y Vie
+ZONA METRO,32792,PEREIRO DE AGUIAR,8,Lun y Vie
+ZONA METRO,32830,A MERCA,7,Lun y Vie
+ZONA METRO,32910,SAN CIBRAO DAS VIÑAS,7,Lun y Vie
+ZONA METRO,32172,AMOEIRO,6,Lun y Vie
+ZONA METRO,32793,PEREIRO DE AGUIAR,6,Lun y Vie
+ZONA METRO,32930,TOEN,6,Lun y Vie
+ZONA METRO,32980,AMOEIRO,6,Lun y Vie
+ZONA METRO,32002,BARBADAS,5,Lun y Vie
+ZONA METRO,32152,COLES,5,Lun y Vie
+ZONA METRO,32100,COLES,4,Lun y Vie
+ZONA METRO,32710,CORTIÑAS PEREIRO DE AGUIAR,4,Lun y Vie
+ZONA METRO,32890,OURENSE,4,Lun y Vie
+ZONA METRO,32920,TOEN,4,Lun y Vie
+ZONA METRO,32710,O PEREIRO DE AGUIAR,3,Lun y Vie
+ZONA METRO,32830,A MANCHICA - A MERCA,3,Lun y Vie
+ZONA METRO,32930,BARBADAS,3,Lun y Vie
+ZONA METRO,32930,OURENSE,3,Lun y Vie
+ZONA METRO,32950,COLES,3,Lun y Vie
+ZONA METRO,32950,OURENSE,3,Lun y Vie
+ZONA METRO,32981,OURENSE,3,Lun y Vie
+ZONA METRO,32001,BARBADAS,2,Lun y Vie
+ZONA METRO,32002,A VALENZA,2,Lun y Vie
+ZONA METRO,32002,CONCELLO DE OURENSE,2,Lun y Vie
+ZONA METRO,32100,OURENSE,2,Lun y Vie
+ZONA METRO,32160,NOGUEIRA DE RAMUIN,2,Lun y Vie
+ZONA METRO,32160,NOGUEIRA DE RAMUÍN,2,Lun y Vie
+ZONA METRO,32161,NOGUEIRA DE RAMUIN,2,Lun y Vie
+ZONA METRO,32667,OURENSE,2,Lun y Vie
+ZONA METRO,32701,OURENSE,2,Lun y Vie
+ZONA METRO,32792,O PEREIRO DE AGUIAR,2,Lun y Vie
+ZONA METRO,32792,OURENSE,2,Lun y Vie
+ZONA METRO,32811,OURENSE,2,Lun y Vie
+ZONA METRO,32890,OURENSE A VALENZA,2,Lun y Vie
+ZONA METRO,32901,SAN CIBRAO DAS VINAS,2,Lun y Vie
+ZONA METRO,32980,OURENSE,2,Lun y Vie
+ZONA METRO,32002,.OURENSE,1,Lun y Vie
+ZONA METRO,32002,"A VALENZA, BARBADAS, OURENSE",1,Lun y Vie
+ZONA METRO,32002,"A VALENZA,BARBADAS",1,Lun y Vie
+ZONA METRO,32005,BARBADAS,1,Lun y Vie
+ZONA METRO,32005,SAN CIBRAO DAS VIÑAS,1,Lun y Vie
+ZONA METRO,32100,CAMBEO/COLES,1,Lun y Vie
+ZONA METRO,32103,OURENSE,1,Lun y Vie
+ZONA METRO,32103,OVISO - COLES,1,Lun y Vie
+ZONA METRO,32112,OURENSE,1,Lun y Vie
+ZONA METRO,32130,OURENSE,1,Lun y Vie
+ZONA METRO,32151,OURENSE,1,Lun y Vie
+ZONA METRO,32152,MERIZ (COLES),1,Lun y Vie
+ZONA METRO,32152,OURENSE,1,Lun y Vie
+ZONA METRO,32160,NOGUEIRA DE RAMIUN,1,Lun y Vie
+ZONA METRO,32161,NOGUEIRA DE RAMUIN - LUINTRA,1,Lun y Vie
+ZONA METRO,32170,AMOEIRO- POVOANZA,1,Lun y Vie
+ZONA METRO,32170,OUTEIRO PARADA DE AMOEIRO,1,Lun y Vie
+ZONA METRO,32170,PARADA DE AMOEIRO,1,Lun y Vie
+ZONA METRO,32172,FIGUEIRAS / AMOEIRO,1,Lun y Vie
+ZONA METRO,32212,OURENSE,1,Lun y Vie
+ZONA METRO,32213,OURENSE,1,Lun y Vie
+ZONA METRO,32229,OURENSE,1,Lun y Vie
+ZONA METRO,32235,OURENSE,1,Lun y Vie
+ZONA METRO,32235,TRADO PONTE DEVA OURENSE,1,Lun y Vie
+ZONA METRO,32236,OURENSE,1,Lun y Vie
+ZONA METRO,32400,OURENSE,1,Lun y Vie
+ZONA METRO,32413,OURENSE,1,Lun y Vie
+ZONA METRO,32418,OURENSE,1,Lun y Vie
+ZONA METRO,32430,OURENSE,1,Lun y Vie
+ZONA METRO,32440,OURENSE,1,Lun y Vie
+ZONA METRO,32448,NOGUEIRA DE RAMUIN,1,Lun y Vie
+ZONA METRO,32454,CENLLE - OURENSE,1,Lun y Vie
+ZONA METRO,32456,OURENSE,1,Lun y Vie
+ZONA METRO,32520,OURENSE,1,Lun y Vie
+ZONA METRO,32539,OURENSE,1,Lun y Vie
+ZONA METRO,32573,OURENSE,1,Lun y Vie
+ZONA METRO,32613,OIMBRA - SAN CIBRAO,1,Lun y Vie
+ZONA METRO,32625,OURENSE,1,Lun y Vie
+ZONA METRO,32643,OURENSE,1,Lun y Vie
+ZONA METRO,32650,OURENSE,1,Lun y Vie
+ZONA METRO,32651,OURENSE,1,Lun y Vie
+ZONA METRO,32652,OURENSE,1,Lun y Vie
+ZONA METRO,32660,OURENSE,1,Lun y Vie
+ZONA METRO,32670,OURENSE,1,Lun y Vie
+ZONA METRO,32695,OURENSE,1,Lun y Vie
+ZONA METRO,32698,OURENSE,1,Lun y Vie
+ZONA METRO,32701,"BAÑOS DE MOLGAS, OURENSE",1,Lun y Vie
+ZONA METRO,32701,BAÑOS DE MOLGAS-OURENSE,1,Lun y Vie
+ZONA METRO,32702,OURENSE,1,Lun y Vie
+ZONA METRO,32704,OURENSE,1,Lun y Vie
+ZONA METRO,32710,CORTIÑA PEREIRO DE AGUIAR,1,Lun y Vie
+ZONA METRO,32710,OURENSE,1,Lun y Vie
+ZONA METRO,32710,PEREIRO DE AGUAR,1,Lun y Vie
+ZONA METRO,32710,PEREIRO DE AGUIAR - OURENSE,1,Lun y Vie
+ZONA METRO,32710,PEREIRO DE GUIAR1,1,Lun y Vie
+ZONA METRO,32711,CONCELLO DE PEREIRO DE AGUIAR,1,Lun y Vie
+ZONA METRO,32711,OURENSE,1,Lun y Vie
+ZONA METRO,32720,OURENSE,1,Lun y Vie
+ZONA METRO,32730,OURENSE,1,Lun y Vie
+ZONA METRO,32748,OURENSE,1,Lun y Vie
+ZONA METRO,32764,OURENSE,1,Lun y Vie
+ZONA METRO,32780,A POBRA DE TRIVES ( OURENSE),1,Lun y Vie
+ZONA METRO,32791,PEREIRO DE AGUIAR,1,Lun y Vie
+ZONA METRO,32792,PEREIRO DE AGUIAR (OS GOZOS),1,Lun y Vie
+ZONA METRO,32792,SANTA MARTA DE MOREIRAS PEREIRO DE AGUIA,1,Lun y Vie
+ZONA METRO,32793,AYUNTAMIENTO DE PEREIRO DE AGUIAR,1,Lun y Vie
+ZONA METRO,32793,O PEREIRO DE AGUIAR,1,Lun y Vie
+ZONA METRO,32793,TAPADA DE BOUZAS-PEREIRO DE AGUIAR,1,Lun y Vie
+ZONA METRO,32794,OURENSE,1,Lun y Vie
+ZONA METRO,32812,OURENSE,1,Lun y Vie
+ZONA METRO,32813,OURENSE,1,Lun y Vie
+ZONA METRO,32814,QUINTELA  DE LEIRADO - OURENSE,1,Lun y Vie
+ZONA METRO,32817,OURENSE,1,Lun y Vie
+ZONA METRO,32823,OURENSE,1,Lun y Vie
+ZONA METRO,32825,OURENSE,1,Lun y Vie
+ZONA METRO,32826,OURENSE,1,Lun y Vie
+ZONA METRO,32830,FORXAS DAS VIÑAS A MERCA,1,Lun y Vie
+ZONA METRO,32830,OURENSE,1,Lun y Vie
+ZONA METRO,32838,"SAN PEDRO DA MEZQUITA, A MERCA",1,Lun y Vie
+ZONA METRO,32839,A MERCA,1,Lun y Vie
+ZONA METRO,32839,ZARRACOS - A MERCA,1,Lun y Vie
+ZONA METRO,32840,OURENSE,1,Lun y Vie
+ZONA METRO,32890,LA VALENZANA - BARBADAS - OURENSE,1,Lun y Vie
+ZONA METRO,32900,POLÍGONO SAN CIBRAO DAS VIÑAS,1,Lun y Vie
+ZONA METRO,32900,SAN CIBRAN DAS VINHAS-OURENSE,1,Lun y Vie
+ZONA METRO,32900,SAN CIBRAO DAS VIÑAS,1,Lun y Vie
+ZONA METRO,32901,OURENSE,1,Lun y Vie
+ZONA METRO,32901,SAN CIBRAO DAS VIÑAS,1,Lun y Vie
+ZONA METRO,32910,SAN CIBRAO,1,Lun y Vie
+ZONA METRO,32911,O PICOUTO SAN CIBRAO,1,Lun y Vie
+ZONA METRO,32911,O PIÑEIRAL SAN CIBRAO,1,Lun y Vie
+ZONA METRO,32911,OURENSE,1,Lun y Vie
+ZONA METRO,32911,SAN CIBRAO,1,Lun y Vie
+ZONA METRO,32911,SAN CIBRAO DAS VINAS,1,Lun y Vie
+ZONA METRO,32915,OURENSE,1,Lun y Vie
+ZONA METRO,32915,SAN CIBRAO DAS VIÑAS,1,Lun y Vie
+ZONA METRO,32930,MUGARES TOEN,1,Lun y Vie
+ZONA METRO,32930,SANTA MARIA DE XESTOSA (TOEN),1,Lun y Vie
+ZONA METRO,32930,XESTOSA TOEN,1,Lun y Vie
+ZONA METRO,32940,CONCELLO DE TOEN,1,Lun y Vie
+ZONA METRO,32940,OURENSE,1,Lun y Vie
+ZONA METRO,32940,"PUGA,TOEN",1,Lun y Vie
+ZONA METRO,32940,TOEN,1,Lun y Vie
+ZONA METRO,32960,OURENSE,1,Lun y Vie
+ZONA METRO,32960,OURENSE A PEDRA,1,Lun y Vie
+ZONA METRO,32980,"PARROQUÍA CASTRO DE BEIRO, OURENSE",1,Lun y Vie
+ZONA METRO,32980,VENDANOVA -AMOEIRO,1,Lun y Vie
+ZONA METRO,32981,BEIRO-OURENSE,1,Lun y Vie
+ZONA METRO,32990,OURENSE,1,Lun y Vie"""
+
+# --- 1. INICIALIZAR MEMORIA ---
+if 'target_coords' not in st.session_state: st.session_state['target_coords'] = None
+if 'target_name' not in st.session_state: st.session_state['target_name'] = ""
+if 'km_result' not in st.session_state: st.session_state['km_result'] = 0
+if 'tipo_calculo' not in st.session_state: st.session_state['tipo_calculo'] = "" 
+if 'zona_info' not in st.session_state: st.session_state['zona_info'] = None
+if 'logistica_transporte' not in st.session_state: st.session_state['logistica_transporte'] = ""
+if 'logistica_frecuencia' not in st.session_state: st.session_state['logistica_frecuencia'] = ""
+if 'logistica_acarreo' not in st.session_state: st.session_state['logistica_acarreo'] = ""
+if 'historial_ruta' not in st.session_state: st.session_state['historial_ruta'] = ""
+if 'tipo_transporte_seleccionado' not in st.session_state: st.session_state['tipo_transporte_seleccionado'] = ""
+
+# --- 2. CARGAR HISTORIAL ---
+@st.cache_data
+def cargar_historial():
+    try:
+        df = pd.read_csv(io.StringIO(CSV_DATA))
+        df['Código postal envío'] = df['Código postal envío'].astype(str)
+        # Limpieza para búsqueda por nombre
+        df['Ciudad_Clean'] = df['Ciudad_Clean'].astype(str).str.strip().str.upper()
+        return df
+    except Exception as e:
+        st.error(f"Error cargando historial: {e}")
+        return None
+
+df_historial = cargar_historial()
+origen_ourense = (42.3358, -7.8639)
+
+# --- 3. FUNCIONES DE CÁLCULO ---
+def obtener_distancia_carretera(origen, destino):
+    url = f"http://router.project-osrm.org/route/v1/driving/{origen[1]},{origen[0]};{destino[1]},{destino[0]}?overview=false"
+    try:
+        r = requests.get(url, timeout=5)
+        if r.status_code == 200:
+            return round(r.json()['routes'][0]['distance'] / 1000, 2), "🚗 Por Carretera"
+    except: pass
+    return round(geodesic(origen, destino).km, 2), "✈️ Línea Recta (Servidor ocupado)"
+
+def calcular_logistica_completa(cp_detectado, nombre_busqueda=""):
+    cp = str(cp_detectado).strip()
+    nombre_busqueda = nombre_busqueda.strip().upper()
+    
+    # 1. BUSCAR EN HISTORIAL (Prioridad Máxima: Por CP o Por Nombre)
+    if df_historial is not None:
+        # Intento A: Por CP exacto
+        match = df_historial[df_historial['Código postal envío'] == cp]
+        
+        # Intento B: Por Nombre (si no hay CP o no hubo match por CP)
+        if match.empty and nombre_busqueda:
+             # Buscamos si el nombre escrito está CONTENIDO en alguna ciudad del excel
+             match = df_historial[df_historial['Ciudad_Clean'].str.contains(nombre_busqueda, na=False)]
+
+        if not match.empty:
+            # Si encontramos coincidencia en el historial
+            dia = match.iloc[0]['Dia_Asignado']
+            ruta = match.iloc[0]['Ruta_Asignada']
+            return "🚛 Transportes Martins", f"📅 {dia}", f"📍 Ruta: {ruta}"
+
+    # 2. SI NO ESTÁ EN HISTORIAL -> Lógica General
+    if not cp or not cp[0].isdigit(): return "❓ Consultar", "Depende destino", "❓"
+    
+    if cp.startswith("32"): return "🚛 Flota Propia / Martins", "⚡ Diaria", "❌ No"
+    elif cp.startswith("36"): return "🚚 Agencia Externa", "📅 L-X-V", "⚠️ Sí (Zona Acarreo)"
+    else: return "✈️ Red Nacional", "⏱️ 48/72h", "⚠️ Sí"
+
+def buscar_con_reintentos(query_dict_or_str, intentos=2):
+    geolocator = Nominatim(user_agent="app_logistica_v11_hybrid")
+    for i in range(intentos):
+        try:
+            if i > 0: time.sleep(1)
+            if isinstance(query_dict_or_str, dict):
+                return geolocator.geocode(query_dict_or_str, timeout=10, addressdetails=True)
+            else:
+                return geolocator.geocode(query_dict_or_str, timeout=10, addressdetails=True)
+        except: continue
+    return None
+
+# --- 4. INTERFAZ ---
+col_izq, col_der = st.columns([1, 2])
+
+with col_izq:
+    st.subheader("📍 Datos del Envío")
+    entrada = st.text_input("Destino (CP, Pueblo...):", placeholder="Ej: 27400 o Monforte")
+    
+    # SELECCIÓN DE TIPO DE TRANSPORTE
+    tipo_camion = st.selectbox("Selecciona Tipo Transporte:", ["ACARREO", "GRUA", "PEQUEÑO"])
+    
+    btn_calc = st.button("🔍 Calcular Ruta", type="primary")
+
+    if btn_calc and entrada:
+        st.session_state['tipo_transporte_seleccionado'] = tipo_camion
+        
+        with st.spinner("Buscando en historial y satélite..."):
+            target_coords, target_name, cp_final = None, "", ""
+            
+            # --- FASE 1: BÚSQUEDA RÁPIDA EN HISTORIAL (Por Nombre) ---
+            # Si el usuario escribe "Monforte", miramos antes de llamar a Internet
+            historial_match = pd.DataFrame()
+            if not entrada.isdigit() and df_historial is not None:
+                 historial_match = df_historial[df_historial['Ciudad_Clean'].str.contains(entrada.strip().upper(), na=False)]
+            
+            # Si encontramos el nombre en el Excel, usamos ese CP para guiar al GPS
+            query_gps = entrada # Por defecto buscamos lo que escribió el usuario
+            if not historial_match.empty:
+                # ¡Bingo! Encontrado en historial por nombre.
+                # Usamos el CP del historial para que el GPS no falle
+                cp_del_historial = str(historial_match.iloc[0]['Código postal envío'])
+                query_gps = {"postalcode": cp_del_historial, "country": "Spain"}
+                cp_final = cp_del_historial # Ya tenemos el CP seguro
+            
+            # --- FASE 2: GEOLOCALIZACIÓN (GPS) ---
+            loc = None
+            # Si ya definimos un query exacto (CP) lo usamos, si no, probamos lógica estándar
+            if isinstance(query_gps, dict):
+                loc = buscar_con_reintentos(query_gps)
+            elif entrada.isdigit() and len(entrada) == 5:
+                loc = buscar_con_reintentos({"postalcode": entrada, "country": "Spain"})
+                cp_final = entrada
+            else:
+                # Búsqueda libre
+                loc = buscar_con_reintentos(f"{entrada}, España
